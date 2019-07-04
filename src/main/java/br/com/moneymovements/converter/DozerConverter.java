635b88eace310370b66dc30e7a9895678dfe19cd
Ariel <ariel120292@gmail.com>
2019-07-04 08:31:00 -0300

    Configure Dozer Mapper

---
+++ src/main/java/br/com/moneymovements/converter/DozerConverter.java
@@ -0,0 +1,23 @@
+package br.com.moneymovements.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+public class DozerConverter {
+	
+	private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+	
+	public static <O, D> D parseObject (O origin, Class<D> destination) {
+		return mapper.map(origin, destination);
+	}
+	
+	public static <O, D> List<D> parseObjectList (List<O> origin, Class<D> destination) {
+		List<D> destinationObjects = new ArrayList<D>();
+		origin.forEach(o -> destinationObjects.add(mapper.map(o, destination)));
+		return destinationObjects;
+	}
+
+}
