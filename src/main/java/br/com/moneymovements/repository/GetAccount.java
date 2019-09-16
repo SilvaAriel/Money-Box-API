d5971e0dce751eb8cf5b9ebe67606859e784f963
Ariel <ariel120292@gmail.com>
2019-09-16 06:46:16 -0300

    Remove unused import

--- src/main/java/br/com/moneymovements/repository/GetAccount.java
+++ src/main/java/br/com/moneymovements/repository/GetAccount.java
@@ -3,7 +3,6 @@ package br.com.moneymovements.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.moneymovements.domain.Account;
