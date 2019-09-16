5ef742cf462bfbfea7c91ef32070151a3f3e343c
Ariel <ariel120292@gmail.com>
2019-09-16 06:08:51 -0300

    Configure Heroku

--- src/main/java/br/com/moneymovements/controller/AccountController.java
+++ src/main/java/br/com/moneymovements/controller/AccountController.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -73,5 +72,5 @@ public class AccountController {
 		boolean acc = accountService.closeAccount(account.getAccountId());
 		return new ResponseEntity<>(acc, HttpStatus.OK);
 	}
-	
+
 }
